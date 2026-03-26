526b2bb2cf363757f7fd7a2a189984b34ed91983
wesleyEmidio <wesley_esc1987@hotmail.com>
2026-03-26 16:51:05 -0300

    Ajuste

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,33 +1,33 @@
-import { InternalServerErrorException, Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { UserModule } from 'src/user/user.module';
-import { CommonModule } from 'src/common/common.module';
-import { JwtModule } from '@nestjs/jwt';
+// import { InternalServerErrorException, Module } from '@nestjs/common';
+// import { AuthController } from './auth.controller';
+// import { AuthService } from './auth.service';
+// import { UserModule } from 'src/user/user.module';
+// import { CommonModule } from 'src/common/common.module';
+// import { JwtModule } from '@nestjs/jwt';
 
-@Module({
-  imports: [
-    UserModule,
-    CommonModule,
-    JwtModule.registerAsync({
-      useFactory: () => {
-        const secret = process.env.JWT_SECRET;
+// @Module({
+//   imports: [
+//     UserModule,
+//     CommonModule,
+//     JwtModule.registerAsync({
+//       useFactory: () => {
+//         const secret = process.env.JWT_SECRET;
 
-        if (!secret) {
-          throw new InternalServerErrorException(
-            'JWT_SECRET not found in .env',
-          );
-        }
+//         if (!secret) {
+//           throw new InternalServerErrorException(
+//             'JWT_SECRET not found in .env',
+//           );
+//         }
 
-        return {
-          secret,
-          signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1d' },
-        };
-      },
-    }),
-  ],
-  controllers: [AuthController],
-  providers: [AuthService],
-  exports: [],
-})
+//         return {
+//           secret,
+//           signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1d' },
+//         };
+//       },
+//     }),
+//   ],
+//   controllers: [AuthController],
+//   providers: [AuthService],
+//   exports: [],
+// })
 export class AuthModule {}
